ca477be30c218d77943640d0d310d242312ba1fd
Kevin Canel <kevinconk25@gmail.com>
2018-09-24 10:56:30 -0600

    yep

--- app/src/main/java/com/example/kcanel/alarma/MainActivity.java
+++ app/src/main/java/com/example/kcanel/alarma/MainActivity.java
@@ -46,35 +46,4 @@ public class MainActivity extends AppCompatActivity{
 
     }
 
-    /*Recibiendo mensaje con la app abierta*/
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-
-        //Log.d(TAG, "From: " + remoteMessage.getFrom());
-        //Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-        if (remoteMessage.getData().size() > 0) {
-            String image = remoteMessage.getData().get("image").toString();
-            Log.d("HOLAAAA", image);
-            //Picasso.get().load().into(foto);
-            //Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                //scheduleJob();
-            } else {
-                // Handle message within 10 seconds
-                //handleNow();
-            }
-
-        }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-           // Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-        }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
-    }
-
-
 }
